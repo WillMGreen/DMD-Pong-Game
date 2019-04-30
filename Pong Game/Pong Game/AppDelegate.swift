ce8c3df05857e30eb8a6bb7cd800beb9309566ba
William Green (s5066515) <s5066515@mac38576.mac.bournemouth.ac.uk>
2019-04-30 11:14:08 +0100

    Remove comments

--- Pong Game/Pong Game/AppDelegate.swift
+++ Pong Game/Pong Game/AppDelegate.swift
@@ -1,10 +1,3 @@
-//
-//  AppDelegate.swift
-//  Pong Game
-//
-//  Created by William Green (s5066515) on 02/04/2019.
-//  Copyright © 2019 William Green (s5066515). All rights reserved.
-//
 
 import UIKit
 
